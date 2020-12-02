b416f530d121f058084360e60849efc0f2059b39
Ibrahim Jarif <ibrahim@dgraph.io>
2020-12-02 19:51:04 +0530

    Add noThrottle folder

---
+++ noThrottle/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func doWork() {
+	// Simulate some work.
+	time.Sleep(time.Second)
+}
+
+func printStats() {
+	for {
+		time.Sleep(time.Millisecond * 500)
+		fmt.Printf("Active number of goroutines = %+v\n", runtime.NumGoroutine())
+	}
+}
+
+func main() {
+	go printStats()
+
+	for i := 0; i < 100; i++ {
+		fmt.Printf("Processing item number = %+v\n", i)
+		go doWork()
+	}
+
+	time.Sleep(time.Second * 2)
+}
